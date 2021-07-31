a575c34590f1575283c77ebf166150027fb014de
Erik Santana <erik_s_marques@hotmail.com>
2021-07-31 18:48:26 -0300

    feat: header component

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="header">
+    <section class="header__firstColumn">
+      <h1>
+        <a href="/">{title}</a>
+      </h1>
+      <HomeOptionsComponent localization="{localization}" />
+    </section>
+    <section class="header__secondColumn">
+      <SearchBarComponent />
+    </section>
+  </header>
+</template>
+
+<script></script>
