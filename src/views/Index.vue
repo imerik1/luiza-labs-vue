cf8f6836ec26634f11894d636380b06468b2da7b
Erik Santana <erik_s_marques@hotmail.com>
2021-08-01 15:56:45 -0300

    feat: mounted to created

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -44,7 +44,7 @@ export default {
       isLoading: true,
     };
   },
-  mounted() {
+  created() {
     fetch("https://run.mocky.io/v3/66063904-d43c-49ed-9329-d69ad44b885e").then(
       (response) => {
         response.json().then((data) => {
