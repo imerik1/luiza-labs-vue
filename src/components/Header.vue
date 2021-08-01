e497924c04a64b6513c095aa3ddb81ea67a5c8e4
Erik Santana <erik_s_marques@hotmail.com>
2021-08-01 01:13:06 -0300

    feat: heade ready

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,14 +2,45 @@
   <header class="header">
     <section class="header__firstColumn">
       <h1>
-        <a href="/">{title}</a>
+        <a href="/">{{ title }}</a>
       </h1>
-      <HomeOptionsComponent localization="{localization}" />
+      <HomeOptions :localization="localization" />
     </section>
     <section class="header__secondColumn">
-      <SearchBarComponent />
+      <SearchBar />
     </section>
   </header>
 </template>
 
-<script></script>
+<script>
+import HomeOptions from "./HomeOptions";
+import SearchBar from "./SearchBar";
+export default {
+  name: "Header",
+  props: ["title", "localization"],
+  components: {
+    HomeOptions,
+    SearchBar,
+  },
+};
+</script>
+
+<style scoped lang="sass">
+.header
+  display: flex
+  flex-flow: column wrap
+  gap: 1rem
+  color: white
+  background-color: #5a2d82
+  &__firstColumn
+    display: flex
+    flex-flow: row wrap
+    justify-content: space-between
+    align-items: center
+    gap: 1rem
+    width: 100%
+  &__secondColumn
+    display: flex
+    flex-flow: row nowrap
+    justify-content: flex-end
+</style>
---
+++ src/components/OptionList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div className="options__icon">
+    <router-link :to="link">
+      <img height="15" :src="require(`@/assets/img/${src}`)" :alt="alt" />
+      <span>{{ description }}</span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "OptionList",
+  props: ["link", "description", "src", "alt"],
+};
+</script>
+
+<style lang="sass" scoped>
+.options__icon
+  display: flex
+  flex-flow: row nowrap
+  justify-content: center
+  align-items: center
+  height: fit-content
+  a
+    display: flex
+    flex-flow: row wrap
+    justify-content: center
+    align-items: center
+    text-align: center
+    img
+      margin-right: 5px
+    span
+      @media screen and (max-width: 446px)
+        display: none
+</style>
