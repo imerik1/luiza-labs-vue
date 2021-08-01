8f34fdc5f334e9108cfcf2847a7da1fe41f3b323
Erik Santana <erik_s_marques@hotmail.com>
2021-08-01 01:12:49 -0300

    feat: sarch ready

---
+++ src/components/HomeOptions.vue
@@ -0,0 +1,46 @@
+<template>
+  <div className="options">
+    <OptionsList
+      link="/"
+      src="map-marker.png"
+      alt="Icone de
+    localização"
+      :description="'Cidade: ' + localization"
+    />
+    <OptionsList
+      link="/"
+      src="phone.png"
+      alt="Icone de telefone"
+      description="Central de atendimento"
+    />
+    <OptionsList
+      link="/wish-list"
+      src="heart.png"
+      alt="Icone de coração"
+      description="Lista de desejos"
+    />
+  </div>
+</template>
+
+<script>
+import OptionsList from "./OptionList";
+export default {
+  name: "HomeOptions",
+  props: ["localization"],
+  components: {
+    OptionsList,
+  },
+};
+</script>
+
+<style scoped lang="sass">
+.header
+  &__firstColumn
+    .options
+      display: flex
+      align-items: center
+      justify-content: space-between
+      width: 100%
+      max-width: 600px
+      gap: 1rem
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div className="header__secondColumn__searchBar">
+    <img
+      height="15"
+      :src="require(`@/assets/img/search.png`)"
+      alt="Lupa de pesquisa"
+    />
+    <input type="text" placeholder="Buscar.." />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+};
+</script>
+
+<style lang="sass" scoped>
+.header
+  &__secondColumn
+    &__searchBar
+      position: relative
+      width: 100%
+      max-width: 600px
+      img
+        position: absolute
+        left: 10px
+        bottom: 32%
+      input[type="text"]
+        padding: 0.8rem 1rem 0.8rem 2.2rem
+        width: 100%
+</style>
